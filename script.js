47eed5dc81b72ce2620a01796b911f1f0c431540
anavguitar <anavguitar@gmail.com>
2024-04-16 01:04:20 -0700

    added some CSS and a new IMG

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const header = document.getElementById('header');
 //these are new creating/DOM elements
 const newH1 = document.createElement('h1');
 const newIMG1 = document.createElement('img');
-const newIMG2 = document.createElement('img');
+// const newIMG2 = document.createElement('img');
 const newUL = document.createElement('ul');
 const newLI1 = document.createElement('li');
 const newLI2 = document.createElement('li');
@@ -12,22 +12,21 @@ const newA1 = document.createElement('a');
 const newA2 = document.createElement('a');
 const newA3 = document.createElement('a');
 
-
 function headerCreate(){
     const headerIMG1 = header.appendChild(newIMG1);
     const headerUL = header.appendChild(newUL);
     const headerLI1 = headerUL.appendChild(newLI1);
     const headerLI2 = headerUL.appendChild(newLI2);
     const headerLI3 = headerUL.appendChild(newLI3);
-    const headerIMG2 = header.appendChild(newIMG2);
+    // const headerIMG2 = header.appendChild(newIMG2);
     const headerA1 = headerLI1.appendChild(newA1);
     const headerA2 = headerLI2.appendChild(newA2);
     const headerA3 = headerLI3.appendChild(newA3);
-    headerIMG1.src= './images/Fun Cat Rescue_transparent.png';
-    headerIMG2.src= './images/Fun Cat Rescue_transparent.png';
+    headerIMG1.src = './images/logo.png';
+    // headerIMG2.src= './images/logo.png';
     headerA1.innerText = 'Mission Statement';
     headerA2.innerText = 'Cats Rescued';
-    headerA3.innerText = 'Team';
+    headerA3.innerText = 'Team Members';
 }
 
 function navCreate(){}
