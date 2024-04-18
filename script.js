f0b751370037f8f920e3e24a7c594fbf1bc30285
anavguitar <anavguitar@gmail.com>
2024-04-18 11:58:59 -0700

    added font awesome icons, font awesome <link>, deleted some unused JS funcs, added some JS variables and an onlick

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const photoAlbumDiv = document.getElementById('js-photo-album');
 const imgAlbum = document.getElementById('img-album');
+const backAlbumBtn = document.getElementById('back');
+const playAlbumBtn = document.getElementById('play');
+const fwdAlbumBtn = document.getElementById('fwd');
+
+const playClick = playAlbumBtn.addEventListener('click', albumAutoPlay)//finish
 
 const jsPhotoAlbumArr = [
     './images/album/0_pic.jpg',
@@ -15,33 +20,9 @@ function jsPhotoAlbumFunc() {
 
 }
 
-function timer() {
-    let start = Date.now();
-    console.log('starting timer...');
-    setTimeout(() => {
-        let milliSec = Date.now() - start;
-        console.log(`seconds ellapsed = ${Math.floor(milliSec / 1000)}`);
-    }, 2000)
-}
-
-function incrementingCounter(interval) {
-    // Initialize a counter variable
-    let counter = 0;
-    // Function to be executed at every interval
-    const printCount = () => {
-        console.log(`Current count: ${counter}`);
-        counter++;  // Increment the counter inside the function
-    };
-    // Start the interval with the specified delay
-    const intervalId = setInterval(printCount, interval);
-    // Optionally, return the interval ID for stopping later
-    return intervalId;
-}
-
-
 //works, but I dont quite get it
 let arrIndex = 0;
-function intervalCounter() {
+function albumAutoPlay() {
    const intervalId = setInterval(() =>{
     if(arrIndex > 6) {
         clearInterval(intervalId);
@@ -52,6 +33,6 @@ function intervalCounter() {
     console.log(arrIndex);
     imgAlbum.setAttribute('src', `${jsPhotoAlbumArr[arrIndex]}`);
     arrIndex++;
-    },3000)
+    },5000)
 }
 
