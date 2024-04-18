a50dcf66fa1de80af9edf1c986341b59070c5111
anavguitar <anavguitar@gmail.com>
2024-04-18 14:49:54 -0700

    turned array into object array, changed the albumAutoPlay() according to new object array

--- script.js
+++ script.js
@@ -6,34 +6,35 @@ const fwdAlbumBtn = document.getElementById('fwd');
 
 const playClick = playAlbumBtn.addEventListener('click', albumAutoPlay)//finish
 
-const jsPhotoAlbumArr = [
-    './images/album/0_pic.jpg',
-    './images/album/1_pic.jpg',
-    './images/album/2_pic.jpg',
-    './images/album/3_pic.jpg',
-    './images/album/4_pic.jpg',
-    './images/album/5_pic.jpg',
-    './images/album/6_pic.jpg'
+const jsImgAlbumArr = [
+    {id: 0, src: './images/album/0_pic.jpg'},
+    {id: 1, src: './images/album/1_pic.jpg'},
+    {id: 2, src: './images/album/2_pic.jpg'},
+    {id: 3, src: './images/album/3_pic.jpg'},
+    {id: 4, src: './images/album/4_pic.jpg'},
+    {id: 5, src: './images/album/5_pic.jpg'},
+    {id: 6, src: './images/album/6_pic.jpg'},
 ]
 
-function imgAlbumAnimate() {
-    imgAlbum.setAttribute('class', 'img-animation');
-}
+// function imgAlbumAnimate() {
+//     imgAlbum.setAttribute('class', 'img-animation');
+// }
 
 //works, but I dont quite get it
-let arrIndex = 0;
+
 function albumAutoPlay() {
+    let arrIndex = 0;
     imgAlbum.setAttribute('class', 'img-animation');
     const intervalId = setInterval(() => {
         if (arrIndex > 6) {
             clearInterval(intervalId);
             arrIndex = 0;
-            imgAlbum.setAttribute('src', `${jsPhotoAlbumArr[0]}`)
+            imgAlbum.setAttribute('src', `${jsImgAlbumArr[6].src}`)
             imgAlbum.setAttribute('class', 'color-border');
             return;
         }
         console.log(arrIndex);//dont NEED this
-        imgAlbum.setAttribute('src', `${jsPhotoAlbumArr[arrIndex]}`);
+        imgAlbum.setAttribute('src', `${jsImgAlbumArr[arrIndex].src}`);
         arrIndex++;
     }, 4000)
 }
